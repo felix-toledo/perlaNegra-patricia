840d0bf81f769332765b2b5af05596f0eba63763
Felix Toledo <felixtoledoctes@gmail.com>
2023-03-01 12:23:53 -0300

    icons added, login started

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 import { productsListen } from './pages/products.js';
 import { homePageListen } from './pages/homePage.js';
+import { login } from './pages/login.js';
 
 homePageListen();
 productsListen();
--- pages/login.js
+++ pages/login.js
@@ -1,3 +1,39 @@
+const userButton = document.querySelector("#user-button");
+const userDataContainer = document.querySelector("#user-container")
+let activated = false;
+let botonLogIn;
+let nombre = "feli";
+
+userButton.addEventListener("click", () => {
+    changeClass(userDataContainer,"user-container-hidden", "user-container");
+    userInfo();
+    });
+
 export function login(){
-    alert(hola);
+    
 }
+
+function userInfo(){
+    let nombreDeUsuario = document.createElement('p');
+    nombreDeUsuario.textContent = nombre;
+
+    if (nombre == ""){
+        botonLogIn = document.createElement("button");
+        botonLogIn.textContent = "Ingresar";
+        userDataContainer.appendChild(botonLogIn);
+    } else {
+        userDataContainer.appendChild(nombreDeUsuario);
+    }
+}
+
+function changeClass(container, oldClass, newClass){
+    if (activated == false){
+        container.classList.replace(oldClass, newClass);
+        activated = true;
+    } else if (activated == true) {
+        container.classList.replace(newClass, oldClass);
+        container.innerHTML = '';
+        activated = false;
+    }
+}
+
